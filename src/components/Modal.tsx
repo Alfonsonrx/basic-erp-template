c0a85a7443c6e88ee88c29b2eeb7820fe00ebb4b
[dyad] <git@dyad.sh>
2026-01-15 00:26:38 -0300

    Updated src/components/Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
-import {createPortal} from "react-dom/client";
+import { createPortal } from 'react-dom';
 
 type ModalProps = {
   open: boolean;
